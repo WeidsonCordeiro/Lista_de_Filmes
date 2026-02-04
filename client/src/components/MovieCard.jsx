ee639f8c2cb3d4786df33df5962700c324f68f96
Weidson Cordeiro <weidson.ac@gmail.com>
2026-02-04 17:03:40 +0000

    Refatorando caminho das imagens no front

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -5,12 +5,11 @@ import { Link } from "react-router-dom";
 import { FaStar } from "react-icons/fa";
 
 const MovieCard = ({ movie, showLink = true }) => {
+  const IMAGE_BASE_URL = import.meta.env.VITE_TMDB_IMAGE_BASE_URL;
+
   return (
     <div className="movie-card">
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
-      />
+      <img src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt={movie.title} />
       <h2>{movie.title}</h2>
       <p>
         <FaStar /> {movie.vote_average}
